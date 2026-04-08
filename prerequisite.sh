7ede34be29079ef3f2a5ce2c2020db28b3e102e2
Puskar Roy <roypuskar2003@gmail.com>
2026-04-08 12:39:06 +0530

    add aws cli in prerequisite

--- prerequisite.sh
+++ prerequisite.sh
@@ -5,4 +5,7 @@ sudo apt install -y software-properties-common
 sudo apt install -y ansible python3 python3-boto3
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
 sudo dpkg -i session-manager-plugin.deb
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -o awscliv2.zip
+./aws/install
 sudo apt install -y unzip wget
